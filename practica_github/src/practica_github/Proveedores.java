607fa13609640db707192c35238cf75a13a621d1
Agustin_Reynaud <agustinreynaud@gmail.com>
2020-06-22 18:49:01 -0300

    java

--- practica_github/src/practica_github/Proveedores.java
+++ practica_github/src/practica_github/Proveedores.java
@@ -1,14 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package practica_github;
 
-/**
- *
- * @author Agustín
- */
+
 public class Proveedores {
+    
+    private static String telefono;
+    private static String nit;
+    private static String direccion;
+    private static Integer cantidad;
+    private static String nombreProveedor;
+
+    public Proveedores() {
+    }
+
+    public static String getTelefono() {
+        return telefono;
+    }
+
+    public static void setTelefono(String telefono) {
+        Proveedores.telefono = telefono;
+    }
+
+    public static String getNit() {
+        return nit;
+    }
+
+    public static void setNit(String nit) {
+        Proveedores.nit = nit;
+    }
+
+    public static String getDireccion() {
+        return direccion;
+    }
+
+    public static void setDireccion(String direccion) {
+        Proveedores.direccion = direccion;
+    }
+
+    public static Integer getCantidad() {
+        return cantidad;
+    }
+
+    public static void setCantidad(Integer cantidad) {
+        Proveedores.cantidad = cantidad;
+    }
+
+    public static String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public static void setNombreProveedor(String nombreProveedor) {
+        Proveedores.nombreProveedor = nombreProveedor;
+    }
+    
+    
     
 }
